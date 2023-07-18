a34d905f27ae3c3f95fd6c242dd7b008ac235a46
priya <priyachauhan47365@gmail.com>
2023-07-18 14:02:57 -0600

    Added : EmployeeWageProblem

---
+++ EmpWage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    internal class EmpWage { 
+         
+        public void CheckPresentAbscentUC1 (int FULL_TIME , int  empCheck )
+            
+        {
+            if (empCheck == FULL_TIME)
+            {
+                Console.WriteLine("employee is present");
+            }
+            else
+            {
+                Console.WriteLine("employee is not  present");
+            }
+
+            Console.ReadLine();
+        }
+
+
+        public void CalculateEmpWageUC2(int FULL_TIME, int empCheck, int EMP_RATE_PER_HOUR) {
+            int empHrs, empWage;
+            
+
+            if(empCheck == FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("EmpWage: " + empWage);
+            Console.ReadLine();
+
+        }
+        public void AddPartTimeEmpWageUC3(int FULL_TIME, int empCheck, int EMP_RATE_PER_HOUR, int PART_TIME )
+        {
+            int empHrs, empWage;
+          
+            if (empCheck == PART_TIME) { 
+                empHrs = 4;
+            }
+
+            else if (empCheck == FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            
+            Console.WriteLine("EmpWage: " + empWage);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+          
+        {
+            int FULL_TIME = 1;
+           // int FULL_TIME = 2;
+            int PART_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            Random empCheck = new Random();
+
+            //Console.WriteLine(empCheck.Next(0,2));
+            
+            EmpWage    employeeWageObj = new EmpWage();
+
+           //  employeeWageObj.CheckPresentAbscentUC1(FULL_TIME, empCheck.Next(0,2));
+
+            employeeWageObj.CalculateEmpWageUC2(FULL_TIME, empCheck.Next(0,2), EMP_RATE_PER_HOUR);
+
+           // employeeWageObj.AddPartTimeEmpWageUC3(FULL_TIME, empCheck.Next(0, 3), EMP_RATE_PER_HOUR, PART_TIME);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
